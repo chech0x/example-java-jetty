36a34c879eed21c07429eaef284a93765cd2a9f4
Sergio Campos <scampos@klabs.cl>
2016-12-21 19:12:09 +0000

    Se modifica glosa por defecto de variable POWERED_BY a NN

--- src/main/java/org/example/Index.java
+++ src/main/java/org/example/Index.java
@@ -12,7 +12,7 @@ public class Index extends HttpServlet {
           throws ServletException, IOException {
       String message = System.getenv("POWERED_BY");
       if (message == null) {
-          message = "Deis";
+          message = "NN";
       }
 
       String release = System.getenv("WORKFLOW_RELEASE");
